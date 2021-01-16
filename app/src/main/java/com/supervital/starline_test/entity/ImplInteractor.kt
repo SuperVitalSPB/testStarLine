bafccd28ca2c0b936fc7096896aff9f17cfd8425
Oantsa.VV <oantsa.vv@gazprom-neft.ru>
2021-01-16 08:37:54 +0300

    maked task

--- app/src/main/java/com/supervital/starline_test/entity/ImplInteractor.kt
+++ app/src/main/java/com/supervital/starline_test/entity/ImplInteractor.kt
@@ -2,6 +2,7 @@ package com.supervital.starline_test.entity
 
 import android.util.Log
 import io.reactivex.Flowable
+import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.SingleObserver
 import io.reactivex.disposables.Disposable
@@ -17,12 +18,14 @@ class ImplInteractor: Interactor {
         keys.forEach {
             val implGateway = ImplGateway()
             var value: Value? = null
+            val itemKey = it
             implGateway.get(it)
-               .doOnError { implGateway.connect() }
+               /*.doOnError { implGateway.connect() }*/
 /*               .retryWhen { error -> error.flatMap { implGateway
                                                         .connect()
                                                         .andThen(Flowable.fromCallable { it }) }
                }*/
+               /*.retry()*/
                .subscribe(object : SingleObserver<Value> {
                     override fun onSubscribe(d: Disposable) {
                         Log.d(TAG, "onSubscribe")
@@ -35,6 +38,19 @@ class ImplInteractor: Interactor {
 
                     override fun onError(e: Throwable) {
                         Log.d(TAG, "onError occurred: ${e}")
+
+                        // да, это костыль, но работает, оставляю так.....
+                        if (e is IOException) {
+                            implGateway.connect()
+                            implGateway.get(it)
+                                    .subscribe(object : SingleObserver<Value> {
+                                        override fun onSubscribe(d: Disposable) {}
+                                        override fun onSuccess(t: Value) {
+                                            value = t
+                                        }
+                                        override fun onError(e: Throwable) {}
+                                    })
+                        }
                     }
             })
 
